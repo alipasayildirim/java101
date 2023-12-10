93c8c02604ac2545bd772743f5c90970ae7db395
Ali Paşa Yıldırım <alipasayildirimm@gmail.com>
2023-12-10 16:13:16 +0300

    Firs upload

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int mat,fizik,kimya,turkce;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Matematik notunuzu giriniz.");
+         mat = input.nextInt();
+
+        System.out.println("Fİzik notunuzu giriniz.");
+        fizik = input.nextInt();
+
+        System.out.println("Kimya notunuzu giriniz.");
+        kimya = input.nextInt();
+
+        System.out.println("Türkçe notunuzu giriniz.");
+        turkce = input.nextInt();
+
+        int toplam  = mat + fizik + kimya + turkce;
+        double sonuc = toplam / 4;
+        System.out.println(sonuc);
+    }
+}
