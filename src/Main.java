61181b330860d4f374e35cc3c0ef865f89acc97a
Ali Paşa Yıldırım <alipasayildirimm@gmail.com>
2023-12-10 16:13:16 +0300

    Update

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+<<<<<<< HEAD
         int mat,fizik,kimya,turkce;
 
         Scanner input = new Scanner(System.in);
 
+=======
+        Scanner input = new Scanner(System.in);
+
+        /* Not Ortalamsı Hesaplama
+
+        int mat,fizik,kimya,turkce;
+
+>>>>>>> 4395972 (Update)
         System.out.println("Matematik notunuzu giriniz.");
          mat = input.nextInt();
 
@@ -22,5 +31,31 @@ public class Main {
         int toplam  = mat + fizik + kimya + turkce;
         double sonuc = toplam / 4;
         System.out.println(sonuc);
+<<<<<<< HEAD
+=======
+
+    */
+
+
+        double price;
+        double kdv = 1.18;
+        double kdv2 = 1.08;
+        double toplam;
+        System.out.println("Ürünün Fiyatını giriniz");
+        price = input.nextDouble();
+
+        if (price == 0 || price <= 1000 ) {
+            toplam = price * kdv;
+            System.out.println("Toplam Fiyat : " + toplam);
+        } else if (price > 1000) {
+            toplam = price * kdv2;
+            System.out.println("Toplam Fiyat : " + toplam);
+        }else {
+            System.out.println("Lütfen girdiğiniz değeri kontrol ediniz.");
+        }
+
+
+
+>>>>>>> 4395972 (Update)
     }
 }
