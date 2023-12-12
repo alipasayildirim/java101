6cc6ea5deac2e4d37770d6c5d78b8ea283e88965
Ali Paşa Yıldırım <alipasayildirimm@gmail.com>
2023-12-12 23:06:21 +0300

    Update

--- src/Main.java
+++ src/Main.java
@@ -214,6 +214,7 @@ public class Main {
 
          */
 
+        /* Etkinlik Önerisi
         int heat;
 
         System.out.print("Sıcaklığı giriniz : ");
@@ -230,5 +231,37 @@ public class Main {
             }else {
                 System.out.println("Yüzmeye gidebilirsiniz.");
             }
-    }
+
+         */
+
+        int a,b,c;
+        System.out.print("a sayısını giriniz : ");
+        a = input.nextInt();
+        System.out.print("b sayısını giriniz : ");
+        b = input.nextInt();
+        System.out.print("c sayısını giriniz : ");
+        c = input.nextInt();
+
+        if ((a > b) && (a > c)) {
+            if (b > c ){
+                System.out.println("a>b>c");
+            }else {
+                System.out.println("a > c > b ");
+            }
+        } else if ((b > a) && (b > c)) {
+            if (a>c) {
+                System.out.println("b>a>c");
+            }else {
+                System.out.println("b>c>a");
+            }
+        } else if ((c > a) && (c > b)) {
+            if (b>a) {
+                System.out.println("c>b>a");
+            }else {
+                System.out.println("c>a>b");
+            }
+        }
+
+
+        }
     }
